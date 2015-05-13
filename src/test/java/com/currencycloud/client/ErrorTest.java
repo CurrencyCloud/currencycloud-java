2fce1295410c43b8d95c98b7ff5d2dbcf3c04e4a
Matija Mazi <matija.mazi@gmail.com>
2015-05-13 12:03:47 +0200

    Model ErrorTest to resemble errors_spec.rb more closely

--- src/test/java/com/currencycloud/client/ErrorTest.java
+++ src/test/java/com/currencycloud/client/ErrorTest.java
@@ -7,6 +7,12 @@ import com.currencycloud.client.model.ErrorMessage;
 import org.junit.Test;
 import org.testng.Assert;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.hasEntry;
+
 @SuppressWarnings({"ThrowableResultOfMethodCallIgnored", "UnnecessaryBoxing"})
 public class ErrorTest extends BetamaxTestSupport {
 
@@ -19,7 +25,6 @@ public class ErrorTest extends BetamaxTestSupport {
         loginId = "non-existent-login-id";
         apiKey = "ef0fd50fca1fb14c1fab3a8436b9ecb57528f0";
         CurrencyCloudException error = testFailedLogin("auth_invalid_user_login_details", 400);
-        Assert.assertEquals(error.getErrorMessages().get("api_key").size(), 1);
         Assert.assertEquals(error.getErrorMessages().get("api_key").get(0).getCode(), "api_key_length_is_invalid");
         Assert.assertEquals(error.getErrorMessages().get("api_key").get(0).getMessage(), "api_key should be 64 character(s) long");
         Assert.assertEquals(error.getErrorMessages().get("api_key").get(0).getParams().get("length"), new Integer(64));
@@ -31,7 +36,6 @@ public class ErrorTest extends BetamaxTestSupport {
         loginId = "non-existent-login-id";
         apiKey = "ef0fd50fca1fb14c1fab3a8436b9ecb57528f0";
         CurrencyCloudException error = testFailedLogin("auth_invalid_user_login_details", 400);
-        Assert.assertEquals(error.getErrorMessages().get("api_key").size(), 1);
         ErrorMessage errorMessage = error.getErrorMessages().get("api_key").get(0);
         Assert.assertEquals(errorMessage.getCode(), "api_key_length_is_invalid");
         Assert.assertEquals(errorMessage.getMessage(), "api_key should be 64 character(s) long");
@@ -42,37 +46,24 @@ public class ErrorTest extends BetamaxTestSupport {
     @Betamax(tape = "is_raised_on_a_forbidden_request", match = {MatchRule.method, MatchRule.uri, MatchRule.body})
     public void testIsRaisedOnAForbiddenRequest() throws Exception {
         CurrencyCloudException error = testFailedLogin("auth_failed", 403);
-/*
-        // todo: use hamcrest or similar assertion library
-        expect(e.code).to eq('auth_failed')
-        expect(e.raw_response).to_not be_nil
-        expect(e.status_code).to eq(403)
-        expect(e.messages.length).to eq(1)
+        assertThat(error.getErrorCode(), equalTo("auth_failed"));
+        assertThat(error.getHttpStatusCode(), equalTo(403));
 
-        error_message = e.messages[0]
-        expect(error_message.field).to eq('username')
-        expect(error_message.code).to eq('invalid_supplied_credentials')
-        expect(error_message.message).to eq('Authentication failed with the supplied credentials')
-        expect(error_message.params).to be_empty
-*/
+
+        ErrorMessage errorMessage = error.getErrorMessages().get("username").get(0);
+        assertThat(errorMessage.getCode(), equalTo("invalid_supplied_credentials"));
+        assertThat(errorMessage.getMessage(), equalTo("Authentication failed with the supplied credentials"));
+        assertThat(errorMessage.getParams(), is(anEmptyMap()));
     }
 
     @Test
     @Betamax(tape = "is_raised_on_an_internal_server_error", match = {MatchRule.method, MatchRule.uri, MatchRule.body})
     public void testIsRaisedOnAnInternalServerError() throws Exception {
         CurrencyCloudException error = testFailedLogin("internal_application_error", 500);
-/*
-        expect(error.code).to eq('internal_application_error')
-        expect(error.raw_response).to_not be_nil
-        expect(error.status_code).to eq(500)
-        expect(error.messages.length).to eq(1)
-
-        error_message = error.messages[0]
-        expect(error_message.field).to eq('base')
-        expect(error_message.code).to eq('internal_application_error')
-        expect(error_message.message).to eq('A general application error occurred')
-        expect(error_message.params).to include("request_id" => 2771875643610572878)
-*/
+        ErrorMessage errorMessage = error.getErrorMessages().get("base").get(0);
+        assertThat(errorMessage.getCode(), equalTo("internal_application_error"));
+        assertThat(errorMessage.getMessage(), equalTo("A general application error occurred"));
+        assertThat(errorMessage.getParams(), hasEntry("request_id", (Object)2771875643610572878L));
     }
 
     @Test
@@ -92,18 +83,10 @@ public class ErrorTest extends BetamaxTestSupport {
     public void testIsRaisedWhenTooManyRequestsHaveBeenIssued() throws Exception {
         loginId = "rjnienaber@gmail.com2";
         CurrencyCloudException error = testFailedLogin("too_many_requests", 429);
-/*
-        expect(error.code).to eq('too_many_requests')
-        expect(error.raw_response).to_not be_nil
-        expect(error.status_code).to eq(429)
-        expect(error.messages.length).to eq(1)
-
-        error_message = error.messages[0]
-        expect(error_message.field).to eq('base')
-        expect(error_message.code).to eq('too_many_requests')
-        expect(error_message.message).to eq('Too many requests have been made to the api. Please refer to the Developer Center for more information')
-        expect(error_message.params).to be_empty
-*/
+        ErrorMessage errorMessage = error.getErrorMessages().get("base").get(0);
+        assertThat(errorMessage.getCode(), equalTo("too_many_requests"));
+        assertThat(errorMessage.getMessage(), equalTo("Too many requests have been made to the api. Please refer to the Developer Center for more information"));
+        assertThat(errorMessage.getParams(), is(anEmptyMap()));
     }
 
     // todo: handling of timout errors
@@ -114,11 +97,11 @@ public class ErrorTest extends BetamaxTestSupport {
         try {
             client.authenticate(loginId, apiKey);
             throw new AssertionError("Should have failed");
-        } catch (CurrencyCloudException e) {
-            Assert.assertEquals(e.getHttpStatusCode(), httpStatusCode);
-            Assert.assertEquals(e.getErrorCode(), errorCode);
-            Assert.assertEquals(e.getErrorMessages().size(), 1);
-            return e;
+        } catch (CurrencyCloudException error) {
+            assertThat(error.getHttpStatusCode(), equalTo(httpStatusCode));
+            assertThat(error.getErrorCode(), equalTo(errorCode));
+            assertThat(error.getErrorMessages().size(), equalTo(1));
+            return error;
         }
     }
 }
