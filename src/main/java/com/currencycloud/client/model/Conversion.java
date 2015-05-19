48336ad22610afa6a33b5f13635957fd051d7479
Matija Mazi <matija.mazi@gmail.com>
2015-05-19 14:59:01 +0200

    Remove builder methods

--- src/main/java/com/currencycloud/client/model/Conversion.java
+++ src/main/java/com/currencycloud/client/model/Conversion.java
@@ -50,11 +50,6 @@ public class Conversion {
         this.id = id;
     }
 
-    public Conversion withId(String id) {
-        this.id = id;
-        return this;
-    }
-
     public String getAccountId() {
         return accountId;
     }
@@ -62,23 +57,13 @@ public class Conversion {
     public void setAccountId(String accountId) {
         this.accountId = accountId;
     }
-
-    public Conversion withAccountId(String accountId) {
-        this.accountId = accountId;
-        return this;
-    }
-
+    
     public String getCreatorContactId() {
         return creatorContactId;
     }
 
     public void setCreatorContactId(String creatorContactId) {
         this.creatorContactId = creatorContactId;
-    }
-
-    public Conversion withCreatorContactId(String creatorContactId) {
-        this.creatorContactId = creatorContactId;
-        return this;
     }
 
     public String getShortReference() {
@@ -89,22 +74,12 @@ public class Conversion {
         this.shortReference = shortReference;
     }
 
-    public Conversion withShortReference(String shortReference) {
-        this.shortReference = shortReference;
-        return this;
-    }
-
     public Date getSettlementDate() {
         return settlementDate;
     }
 
     public void setSettlementDate(Date settlementDate) {
         this.settlementDate = settlementDate;
-    }
-
-    public Conversion withSettlementDate(Date settlementDate) {
-        this.settlementDate = settlementDate;
-        return this;
     }
 
     public Date getConversionDate() {
@@ -115,22 +90,12 @@ public class Conversion {
         this.conversionDate = conversionDate;
     }
 
-    public Conversion withConversionDate(Date conversionDate) {
-        this.conversionDate = conversionDate;
-        return this;
-    }
-
     public String getStatus() {
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public Conversion withStatus(String status) {
-        this.status = status;
-        return this;
     }
 
     public String getPartnerStatus() {
@@ -141,22 +106,12 @@ public class Conversion {
         this.partnerStatus = partnerStatus;
     }
 
-    public Conversion withPartnerStatus(String partnerStatus) {
-        this.partnerStatus = partnerStatus;
-        return this;
-    }
-
     public String getCurrencyPair() {
         return currencyPair;
     }
 
     public void setCurrencyPair(String currencyPair) {
         this.currencyPair = currencyPair;
-    }
-
-    public Conversion withCurrencyPair(String currencyPair) {
-        this.currencyPair = currencyPair;
-        return this;
     }
 
     public String getBuyCurrency() {
@@ -167,22 +122,12 @@ public class Conversion {
         this.buyCurrency = buyCurrency;
     }
 
-    public Conversion withBuyCurrency(String buyCurrency) {
-        this.buyCurrency = buyCurrency;
-        return this;
-    }
-
     public String getSellCurrency() {
         return sellCurrency;
     }
 
     public void setSellCurrency(String sellCurrency) {
         this.sellCurrency = sellCurrency;
-    }
-
-    public Conversion withSellCurrency(String sellCurrency) {
-        this.sellCurrency = sellCurrency;
-        return this;
     }
 
     public String getFixedSide() {
@@ -193,22 +138,12 @@ public class Conversion {
         this.fixedSide = fixedSide;
     }
 
-    public Conversion withFixedSide(String fixedSide) {
-        this.fixedSide = fixedSide;
-        return this;
-    }
-
     public BigDecimal getPartnerBuyAmount() {
         return partnerBuyAmount;
     }
 
     public void setPartnerBuyAmount(BigDecimal partnerBuyAmount) {
         this.partnerBuyAmount = partnerBuyAmount;
-    }
-
-    public Conversion withPartnerBuyAmount(BigDecimal partnerBuyAmount) {
-        this.partnerBuyAmount = partnerBuyAmount;
-        return this;
     }
 
     public BigDecimal getPartnerSellAmount() {
@@ -219,22 +154,12 @@ public class Conversion {
         this.partnerSellAmount = partnerSellAmount;
     }
 
-    public Conversion withPartnerSellAmount(BigDecimal partnerSellAmount) {
-        this.partnerSellAmount = partnerSellAmount;
-        return this;
-    }
-
     public BigDecimal getClientBuyAmount() {
         return clientBuyAmount;
     }
 
     public void setClientBuyAmount(BigDecimal clientBuyAmount) {
         this.clientBuyAmount = clientBuyAmount;
-    }
-
-    public Conversion withClientBuyAmount(BigDecimal clientBuyAmount) {
-        this.clientBuyAmount = clientBuyAmount;
-        return this;
     }
 
     public BigDecimal getClientSellAmount() {
@@ -245,22 +170,12 @@ public class Conversion {
         this.clientSellAmount = clientSellAmount;
     }
 
-    public Conversion withClientSellAmount(BigDecimal clientSellAmount) {
-        this.clientSellAmount = clientSellAmount;
-        return this;
-    }
-
     public BigDecimal getMidMarketRate() {
         return midMarketRate;
     }
 
     public void setMidMarketRate(BigDecimal midMarketRate) {
         this.midMarketRate = midMarketRate;
-    }
-
-    public Conversion withMidMarketRate(BigDecimal midMarketRate) {
-        this.midMarketRate = midMarketRate;
-        return this;
     }
 
     public BigDecimal getCoreRate() {
@@ -271,22 +186,12 @@ public class Conversion {
         this.coreRate = coreRate;
     }
 
-    public Conversion withCoreRate(BigDecimal coreRate) {
-        this.coreRate = coreRate;
-        return this;
-    }
-
     public BigDecimal getPartnerRate() {
         return partnerRate;
     }
 
     public void setPartnerRate(BigDecimal partnerRate) {
         this.partnerRate = partnerRate;
-    }
-
-    public Conversion withPartnerRate(BigDecimal partnerRate) {
-        this.partnerRate = partnerRate;
-        return this;
     }
 
     public BigDecimal getClientRate() {
@@ -297,22 +202,12 @@ public class Conversion {
         this.clientRate = clientRate;
     }
 
-    public Conversion withClientRate(BigDecimal clientRate) {
-        this.clientRate = clientRate;
-        return this;
-    }
-
     public Boolean getDepositRequired() {
         return depositRequired;
     }
 
     public void setDepositRequired(Boolean depositRequired) {
         this.depositRequired = depositRequired;
-    }
-
-    public Conversion withDepositRequired(Boolean depositRequired) {
-        this.depositRequired = depositRequired;
-        return this;
     }
 
     public BigDecimal getDepositAmount() {
@@ -323,22 +218,12 @@ public class Conversion {
         this.depositAmount = depositAmount;
     }
 
-    public Conversion withDepositAmount(BigDecimal depositAmount) {
-        this.depositAmount = depositAmount;
-        return this;
-    }
-
     public String getDepositCurrency() {
         return depositCurrency;
     }
 
     public void setDepositCurrency(String depositCurrency) {
         this.depositCurrency = depositCurrency;
-    }
-
-    public Conversion withDepositCurrency(String depositCurrency) {
-        this.depositCurrency = depositCurrency;
-        return this;
     }
 
     public String getDepositStatus() {
@@ -349,22 +234,12 @@ public class Conversion {
         this.depositStatus = depositStatus;
     }
 
-    public Conversion withDepositStatus(String depositStatus) {
-        this.depositStatus = depositStatus;
-        return this;
-    }
-
     public Date getDepositRequiredAt() {
         return depositRequiredAt;
     }
 
     public void setDepositRequiredAt(Date depositRequiredAt) {
         this.depositRequiredAt = depositRequiredAt;
-    }
-
-    public Conversion withDepositRequiredAt(Date depositRequiredAt) {
-        this.depositRequiredAt = depositRequiredAt;
-        return this;
     }
 
     public List<String> getPaymentIds() {
@@ -375,11 +250,6 @@ public class Conversion {
         this.paymentIds = paymentIds;
     }
 
-    public Conversion withPaymentIds(List<String> paymentIds) {
-        this.paymentIds = paymentIds;
-        return this;
-    }
-
     public Date getCreatedAt() {
         return createdAt;
     }
@@ -388,22 +258,12 @@ public class Conversion {
         this.createdAt = createdAt;
     }
 
-    public Conversion withCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
-
     public Date getUpdatedAt() {
         return updatedAt;
     }
 
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
-    }
-
-    public Conversion withUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-        return this;
     }
 
     @Override
