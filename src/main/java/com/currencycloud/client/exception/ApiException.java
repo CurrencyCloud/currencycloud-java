c25b8d0a238e8ea690b666297fdb6f9f3549683b
Matija Mazi <matija.mazi@gmail.com>
2015-05-28 17:44:56 +0200

    Collect error messages into the exception message

--- src/main/java/com/currencycloud/client/exception/ApiException.java
+++ src/main/java/com/currencycloud/client/exception/ApiException.java
@@ -12,10 +12,23 @@ public class ApiException extends CurrencyCloudException {
     private final Map<String, List<ErrorMessage>> errorMessages;
 
     public ApiException(ResponseException e) {
-        super(e.getMessage());
+        super(collectMessages(e));
         httpStatusCode = e.getHttpStatusCode();
         errorCode = e.getErrorCode();
         errorMessages = e.getErrorMessages();
+    }
+
+    static String collectMessages(ResponseException e) {
+        StringBuilder sb = new StringBuilder();
+        for (List<ErrorMessage> msgs : e.getErrorMessages().values()) {
+            for (ErrorMessage msg : msgs) {
+                if (sb.length() > 0) {
+                    sb.append("; ");
+                }
+                sb.append(msg.getMessage());
+            }
+        }
+        return sb.toString();
     }
 
     public static ApiException create(ResponseException e) {
