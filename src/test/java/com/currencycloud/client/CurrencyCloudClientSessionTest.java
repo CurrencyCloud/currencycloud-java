f5a28f2fa4bf9287136cdb50f666b6accae3faca
Matija Mazi <matija.mazi@gmail.com>
2015-05-28 16:54:57 +0200

    #20: Test create client

---
+++ src/test/java/com/currencycloud/client/CurrencyCloudClientSessionTest.java
@@ -0,0 +1,42 @@
+package com.currencycloud.client;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CurrencyCloudClientSessionTest {
+
+    @Test
+    public void testRaisesAnErrorIfTheEnvironmentIsNotSet() throws Exception {
+        try {
+            CurrencyCloudClient client = new CurrencyCloudClient((CurrencyCloudClient.Environment) null, null, null);
+            client.authenticate();
+            throw new AssertionError("Should have failed.");
+        } catch (NullPointerException ignored) { }
+    }
+
+    @Test
+    public void testRaisesAnErrorIfTheLoginIdIsNotSet() throws Exception {
+        CurrencyCloudClient client = new CurrencyCloudClient(CurrencyCloudClient.Environment.demo, null, null);
+        try {
+            client.authenticate();
+            throw new AssertionError("Should have failed.");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), containsString("apiKey must be set"));
+        }
+    }
+
+    @Test
+    public void testRaisesAnErrorIfTheApiKeyIsNotSet() throws Exception {
+        CurrencyCloudClient client = new CurrencyCloudClient(CurrencyCloudClient.Environment.demo, "test@example.com", null);
+        try {
+            client.authenticate();
+            throw new AssertionError("Should have failed.");
+        } catch (IllegalArgumentException e) {
+            assertThat(e.getMessage(), containsString("apiKey"));
+            assertThat(e.getMessage(), containsString("must be set"));
+        }
+    }
+}
