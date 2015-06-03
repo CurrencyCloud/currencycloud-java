7af634129ca32b575c8140616c19c3edb08e478e
Matija Mazi <matija.mazi@gmail.com>
2015-06-03 15:23:42 +0200

    #30: Fix deploy condition shell script checking

--- after-success.sh
+++ after-success.sh
@@ -4,10 +4,10 @@ JAVA_VERSION=`java -version 2>&1 | sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2
 GIT_BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 
 echo "Java version:        " ${JAVA_VERSION}
-echo "git branch:          " ${GIT_BRANCH}
+echo "Travis branch:       " ${TRAVIS_BRANCH}
 echo "Travis pull request: " ${TRAVIS_PULL_REQUEST}
-if [ ${JAVA_VERSION} == "17" -a ${GIT_BRANCH} == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+if [ "${JAVA_VERSION}" == "17" -a "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     mvn deploy --settings settings.xml
 else
-    echo "Not deploying artefacts. This is only done with commits to master branch with Java 7 builds."
+    echo "Not deploying artefacts. This is only done with non-pull-request commits to master branch with Java 7 builds."
 fi
