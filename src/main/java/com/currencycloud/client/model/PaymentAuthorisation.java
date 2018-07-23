76b3e155a9d2bfc3d376b8f2749b58f00d873e00
igoraptekar <igoraptekar@igors-mbp>
2018-07-23 09:17:21 +0100

    remove equals and hashcode

--- src/main/java/com/currencycloud/client/model/PaymentAuthorisation.java
+++ src/main/java/com/currencycloud/client/model/PaymentAuthorisation.java
@@ -64,50 +64,6 @@ public class PaymentAuthorisation implements Entity {
 	public void setError(String error) {
 		this.error = error;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + authStepsRequired;
-		result = prime * result + authStepsTaken;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((paymentStatus == null) ? 0 : paymentStatus.hashCode());
-		result = prime * result + ((shortReference == null) ? 0 : shortReference.hashCode());
-		result = prime * result + (updated ? 1231 : 1237);
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		PaymentAuthorisation other = (PaymentAuthorisation) obj;
-		if (authStepsRequired != other.authStepsRequired)
-			return false;
-		if (authStepsTaken != other.authStepsTaken)
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (paymentStatus == null) {
-			if (other.paymentStatus != null)
-				return false;
-		} else if (!paymentStatus.equals(other.paymentStatus))
-			return false;
-		if (shortReference == null) {
-			if (other.shortReference != null)
-				return false;
-		} else if (!shortReference.equals(other.shortReference))
-			return false;
-		if (updated != other.updated)
-			return false;
-		return true;
-	}
 
 	@Override
 	public String toString() {
