fa481b296c9cc77d28785a6b4e585f9c98559fc3
Ed Addario <ed.addario@currencycloud.com>
2018-11-05 11:16:21 +0000

    Add Currency missing fields

--- src/main/java/com/currencycloud/client/model/Currency.java
+++ src/main/java/com/currencycloud/client/model/Currency.java
@@ -1,9 +1,15 @@
 package com.currencycloud.client.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import net.minidev.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -13,6 +19,8 @@ public class Currency {
     private Integer decimalPlaces;
     private String name;
     private Boolean onlineTrading;
+    private Boolean canBuy;
+    private Boolean canSell;
 
     public String getCode() {
         return code;
@@ -30,13 +38,31 @@ public class Currency {
         return onlineTrading;
     }
 
+    public Boolean getCanBuy() {
+        return canBuy;
+    }
+
+    public Boolean getCanSell() {
+        return canSell;
+    }
+
     @Override
     public String toString() {
-        return new JSONObject()
-                .appendField("code", code)
-                .appendField("decimalPlaces", decimalPlaces)
-                .appendField("name", name)
-                .appendField("onlineTrading", onlineTrading)
-                .toString();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX"));
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", code);
+        map.put("decimalPlaces", decimalPlaces);
+        map.put("name", name);
+        map.put("onlineTrading", onlineTrading);
+        map.put("canBuy", canBuy);
+        map.put("canSell", canSell);
+
+        try {
+            return objectMapper.writeValueAsString(map);
+        } catch (JsonProcessingException e) {
+            return String.format("{\"error\": \"%s\"}", e.getMessage());
+        }
     }
 }
