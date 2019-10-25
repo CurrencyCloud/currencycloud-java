c51d214cf2989878eebc2b80baa7777122ea7f92
jonathancouchman <jonathan.couchman@currencycloud.com>
2019-10-25 14:31:32 +0100

    Release 3.3.1

--- src/main/java/com/currencycloud/client/CurrencyCloudClient.java
+++ src/main/java/com/currencycloud/client/CurrencyCloudClient.java
@@ -41,7 +41,7 @@ public class CurrencyCloudClient {
             "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
             Pattern.CASE_INSENSITIVE
     );
-    private static final String userAgent = "CurrencyCloudSDK/2.0 Java/3.2.5";
+    private static final String userAgent = "CurrencyCloudSDK/2.0 Java/3.3.1";
 
     private final CurrencyCloud api;
 
