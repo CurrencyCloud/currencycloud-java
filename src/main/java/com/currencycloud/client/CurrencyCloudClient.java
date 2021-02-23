77d19d17eb3c06f680f5b80e2677db6aa030bd47
jonathancouchman <44161978+jonathancouchman@users.noreply.github.com>
2021-02-23 09:14:13 +0000

    Release/4.1.3 (#124)
    * Release 4.1.3
    
    * [maven-release-plugin] prepare release currencycloud-java-4.1.3
    
    * [maven-release-plugin] prepare for next development iteration

--- src/main/java/com/currencycloud/client/CurrencyCloudClient.java
+++ src/main/java/com/currencycloud/client/CurrencyCloudClient.java
@@ -41,7 +41,7 @@ public class CurrencyCloudClient {
             "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
             Pattern.CASE_INSENSITIVE
     );
-    private static final String userAgent = "CurrencyCloudSDK/2.0 Java/4.0.2";
+    private static final String userAgent = "CurrencyCloudSDK/2.0 Java/4.1.3";
 
     private final CurrencyCloud api;
 
