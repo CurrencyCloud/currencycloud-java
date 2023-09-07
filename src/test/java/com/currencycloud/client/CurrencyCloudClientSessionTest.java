ef259b94129a7722b974cc731f490e156d0ce88d
Ross Ackland <ross.ackland@currencycloud.com>
2023-09-07 14:42:25 +0100

    Allowed string to be used for url by making constructor public, added Url validation and unit tests

--- src/test/java/com/currencycloud/client/CurrencyCloudClientSessionTest.java
+++ src/test/java/com/currencycloud/client/CurrencyCloudClientSessionTest.java
@@ -4,39 +4,40 @@ package com.currencycloud.client;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
 
 public class CurrencyCloudClientSessionTest {
 
     @Test
-    public void testRaisesAnErrorIfTheEnvironmentIsNotSet() throws Exception {
-        try {
-            CurrencyCloudClient client = new CurrencyCloudClient((CurrencyCloudClient.Environment) null, null, null);
-            client.authenticate();
-            throw new AssertionError("Should have failed.");
-        } catch (NullPointerException ignored) { }
+    public void testRaisesAnErrorIfTheEnvironmentIsNotSet() {
+        assertThrows(NullPointerException.class, () -> new CurrencyCloudClient((CurrencyCloudClient.Environment) null, null, null));
     }
 
     @Test
-    public void testRaisesAnErrorIfTheLoginIdIsNotSet() throws Exception {
+    public void testRaisesAnErrorIfTheLoginIdIsNotSet() {
         CurrencyCloudClient client = new CurrencyCloudClient(CurrencyCloudClient.Environment.demo, null, null);
-        try {
-            client.authenticate();
-            throw new AssertionError("Should have failed.");
-        } catch (IllegalArgumentException e) {
-            assertThat(e.getMessage(), containsString("apiKey must be set"));
-        }
+        Exception exception = assertThrows(IllegalArgumentException.class, client::authenticate);
+        assertThat(exception.getMessage(), containsString("apiKey must be set"));
     }
 
     @Test
-    public void testRaisesAnErrorIfTheApiKeyIsNotSet() throws Exception {
+    public void testRaisesAnErrorIfTheApiKeyIsNotSet() {
         CurrencyCloudClient client = new CurrencyCloudClient(CurrencyCloudClient.Environment.demo, "development@currencycloud.com", null);
-        try {
-            client.authenticate();
-            throw new AssertionError("Should have failed.");
-        } catch (IllegalArgumentException e) {
-            assertThat(e.getMessage(), containsString("apiKey"));
-            assertThat(e.getMessage(), containsString("must be set"));
-        }
+        Exception exception = assertThrows(IllegalArgumentException.class, client::authenticate);
+        assertThat(exception.getMessage(), containsString("apiKey must be set"));
+    }
+
+    @Test
+    public void testRaisesAnErrorIfTheUrlIncorrect() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new CurrencyCloudClient("example.com", "development@currencycloud.com", "key"));
+        assertThat(exception.getMessage(), equalTo("URI is not absolute"));
+    }
+
+    @Test
+    public void testRaisesAnErrorIfTheApiKeyIsNotSet2() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new CurrencyCloudClient("example com", "development@currencycloud.com", "key"));
+        assertThat(exception.getMessage(), containsString("Illegal character in path at index 7: example com"));
     }
 }
