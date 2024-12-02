2bc7de62e6036b5c0ba3108b0057c762bb6f056b
niall-logue <nlogue@visa.com>
2024-12-02 15:11:12 +0000

    Bugfix/sdkjava-105 update auth token user agent order (#185)
    * SDKJAVA-105: Updated order of user agent and auth token for end session method
    
    * SDKJAVA-105: Incremented patch version

--- src/main/java/com/currencycloud/client/CurrencyCloudClient.java
+++ src/main/java/com/currencycloud/client/CurrencyCloudClient.java
@@ -41,7 +41,7 @@ public class CurrencyCloudClient {
       "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
       Pattern.CASE_INSENSITIVE
   );
-  private static final String userAgent = "CurrencyCloudSDK/2.0 Java/5.9.0";
+  private static final String userAgent = "CurrencyCloudSDK/2.0 Java/5.9.1";
 
   private final CurrencyCloud api;
 
@@ -210,7 +210,7 @@ public class CurrencyCloudClient {
    * Ends a logged in session
    */
   public void endSession() throws CurrencyCloudException {
-    api.endSession(userAgent, authToken);
+    api.endSession(authToken, userAgent);
     authToken = null;
   }
 
